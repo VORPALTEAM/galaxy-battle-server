0e2d126e2d71a6997564d635de18b89d34f8bf7a
Yuriy <berlandspw@gmail.com>
2024-04-21 23:25:59 +0500

    Tests update

--- src/tests.ts
+++ src/tests.ts
@@ -1,10 +1,21 @@
-import { getUserAvailableLaserLevelsWeb2 } from "./blockchain/boxes/boxesweb2.js"
+import dotenv from 'dotenv';
+import { RecordWinnerWithChoose } from "./blockchain/boxes/boxes.js"
+import { GetGameAssetsWeb2, getUserAvailableLaserLevelsWeb2 } from "./blockchain/boxes/boxesweb2.js"
 
+dotenv.config({ path: '.env.production' });
 
 console.log("Tesing...")
 
-const _user = "0xWalletToTests".toLowerCase()
+const _user = "0xAddressToken".toLowerCase()
 
 getUserAvailableLaserLevelsWeb2(_user).then((result) => {
     console.log(result)
+})
+
+RecordWinnerWithChoose(_user, false, "TelegramDebug", 1).then((result) => {
+    console.log(result)
+    console.log("Requesting assets")
+    GetGameAssetsWeb2(_user).then((assets) => {
+        console.log(assets)
+    })
 })
