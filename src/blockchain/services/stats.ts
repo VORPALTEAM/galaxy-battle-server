220427320e68ae8155f13ab7d2d75d7ae96ecf82
Yuriy <berlandspw@gmail.com>
2024-11-19 13:17:28 +0500

    bot stats saving fix

--- src/blockchain/services/stats.ts
+++ src/blockchain/services/stats.ts
@@ -1,5 +1,5 @@
-import { GetSignedAuthMessage } from "../functions";
-import { fastServerUrl } from "../network";
+import { GetSignedAuthMessage } from "../functions.js";
+import { fastServerUrl } from "../network.js";
 import { DuelPlayerStats, PlayerSummaryStats } from "../types";
 
 export class StatService {
@@ -17,7 +17,7 @@ export class StatService {
     public async setStats( stats: DuelPlayerStats[]): Promise<boolean> {
         return new Promise((resolve, reject) => {
             const signMessage = GetSignedAuthMessage();
-            fetch(fastServerUrl.concat('/api/duel/stats/add'), {
+            fetch(fastServerUrl.concat('api/duel/stats/add'), {
                 method: 'post',
                 headers: {
                   "Content-Type": "application/json"
@@ -40,7 +40,7 @@ export class StatService {
     }
     
     public async getStats (category: "duel" | "player", itemId: string): Promise<DuelPlayerStats[]> {
-        const url = fastServerUrl.concat(`/api/duel/${category}/${itemId}`);
+        const url = fastServerUrl.concat(`api/duel/${category}/${itemId}`);
         return new Promise((resolve, reject) => {
             fetch(url).then((res) => {
                 if (res.status !== 200) {
@@ -56,7 +56,7 @@ export class StatService {
     }
     
     public async getPlayerAggregateStats (itemId: string): Promise<PlayerSummaryStats | null> {
-        const url = fastServerUrl.concat(`/api/duel/summary/${itemId}`);
+        const url = fastServerUrl.concat(`api/duel/summary/${itemId}`);
         return new Promise((resolve, reject) => {
             fetch(url).then((res) => {
                 if (res.status !== 200) {
@@ -72,7 +72,7 @@ export class StatService {
     }
 
     public async loadPlayWithBotStats (telegramInitData: string): Promise<any> {
-        const url = fastServerUrl.concat(`/api/playwithbot/set`);
+        const url = fastServerUrl.concat(`api/playwithbot/set`);
         return new Promise((resolve, reject) => {
             fetch(url, {
                 method: "post",
